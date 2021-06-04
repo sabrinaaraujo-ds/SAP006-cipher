4a71863a15400ff57da4f3863b74074522482782
Sabrina Araujo <sabrinaaraujo.ds@gmail.com>
2021-06-04 17:56:59 -0300

    Funçao Encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
+
+
 const cipher = {
-  // ...
+
+    encode(offset, string) {
+
+        var resultado = '';
+    
+         for (let i = 0; i < string.length; i++){
+    
+            var letraAtual = string[i];
+            var ascCode = string.charCodeAt(i);     
+                   
+            //verificando letras maiúsculas  
+            if(ascCode >= 65 && ascCode <= 90){
+                letraAtual = String.fromCharCode((ascCode - 65 + offset) % 26 + 65)
+            }
+            //verificando letras minúsculas 
+            else if (ascCode >= 97 && ascCode <= 122){
+                letraAtual = String.fromCharCode((ascCode - 97 + offset) % 26 + 97)
+            }
+    
+            //alimentando resultado
+            resultado += letraAtual
+        }
+    
+        return resultado;
+    }
+
+    
 };
+
+//console.log(cipher.encode(1,"Casa"))
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const msg = document.getElementById('input-message')
+const key = document.getElementById('key')
+var result = document.getElementById('output-message')
+const btnEncode = document.getElementById('btn-encode')
+
+function getValues(){
+    let string = msg.value
+    let offset = parseInt(key.value)    
+
+    return result.value = cipher.encode(offset, string) 
+}
+
+btnEncode.addEventListener('click', getValues )
+
+//console.log(cipher);
