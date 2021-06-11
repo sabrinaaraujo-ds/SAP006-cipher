4408389750a50210ff9bc873348cd0f67f5e69c6
Sabrina Araujo <sabrinaaraujo.ds@gmail.com>
2021-06-11 11:52:49 -0300

    Funçao Decode

--- src/cipher.js
+++ src/cipher.js
@@ -4,20 +4,47 @@ const cipher = {
 
     encode(offset, string) {
 
-        var resultado = '';
+        let resultado = '';
     
          for (let i = 0; i < string.length; i++){
     
-            var letraAtual = string[i];
-            var ascCode = string.charCodeAt(i);     
+            let letraAtual = string[i];
+            let ascCode = string.charCodeAt(i);     
                    
             //verificando letras maiúsculas  
             if(ascCode >= 65 && ascCode <= 90){
                 letraAtual = String.fromCharCode((ascCode - 65 + offset) % 26 + 65)
             }
+            
             //verificando letras minúsculas 
             else if (ascCode >= 97 && ascCode <= 122){
                 letraAtual = String.fromCharCode((ascCode - 97 + offset) % 26 + 97)
+            }
+    
+            //alimentando resultado
+            resultado += letraAtual
+        }
+    
+        return resultado;
+    },
+
+    decode(offset, string) {
+
+        let resultado = '';
+    
+         for (let i = 0; i < string.length; i++){
+    
+            let letraAtual = string[i];
+            let ascCode = string.charCodeAt(i);     
+                   
+            //verificando letras maiúsculas  
+            if(ascCode >= 65 && ascCode <= 90){
+                letraAtual = String.fromCharCode((ascCode + 65 - offset) % 26 + 65)
+            }
+            
+            //verificando letras minúsculas 
+            if (ascCode >= 97 && ascCode <= 122){
+                letraAtual = String.fromCharCode((ascCode + 97 - offset) % 26 + 97)
             }
     
             //alimentando resultado
@@ -30,6 +57,6 @@ const cipher = {
     
 };
 
-//console.log(cipher.encode(1,"Casa"))
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,37 @@ import cipher from './cipher.js';
 const msg = document.getElementById('input-message')
 const key = document.getElementById('key')
 var result = document.getElementById('output-message')
-const btnEncode = document.getElementById('btn-encode')
 
-function getValues(){
+const btnEncode = document.getElementById('encode-btn')
+const btnDecode = document.getElementById('decode-btn')
+
+//funcao  com novos parametros, inseridos pelo usuario. Retorna a funcao ENCODE que encripta a mensagem
+function getValuesEncode(){
+    //mensagem
     let string = msg.value
+    //offset
     let offset = parseInt(key.value)    
 
-    return result.value = cipher.encode(offset, string) 
+    //console.log(string)
+    //console.log(typeof offset)
+    
+    return result.value = cipher.encode(offset, string)   
 }
 
-btnEncode.addEventListener('click', getValues )
+function getValuesDecode(){
+    let string = msg.value
+    let offset = parseInt(key.value)
+        
+    return result.value = cipher.decode(offset, string)
+}
+
+//dois paramentros: evento e callback Function?
+//quando o botao e clicado, chama a funcao que tem os valores do usuario e tbm o resultado.
+
+//acao para ENCRIPTAR os valores passados pelo usuario
+btnEncode.addEventListener('click', getValuesEncode)
+
+//acao para DECRIPTAR os valores passados pelo usuario
+btnDecode.addEventListener('click', getValuesDecode)
 
 //console.log(cipher);
